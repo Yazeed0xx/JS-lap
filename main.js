4ce3c1d802bca6e1012a15eff4b2aec30d1ccd04
Yazeed0xx <yazeedthunyan@gmail.com>
2024-05-29 04:11:57 +0300

    lap1

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+    var myWindow = window.open("", "", "width=375,height=812");
+  }
+  function myFunction2() {
+    var myWindow = window.open("", "", "width=412,height=869");
+  }
+  function myFunction3() {
+    var myWindow = window.open("", "", "width=1024,height=1366");
+  }
+
+
+function myFunction4() {
+    var myWindow = window.open("", "", "width=1920,height=1080");
+  }
